4a712af725f0f8460ef5f58fdc7568ac27d95936
BaxleyAldworth <jbaxle@gmail.com>
2015-12-04 16:12:04 -0600

    Proper migrations? Yes

---
+++ HackTest2.API/Migrations/201512042210190_Initial-Creation.cs
@@ -0,0 +1,27 @@
+namespace HackTest2.API.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreation : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Author = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Books");
+        }
+    }
+}
--- HackTest2.API/Models/Book.cs
+++ HackTest2.API/Models/Book.cs
@@ -7,6 +7,7 @@ namespace HackTest2.API.Models
 {
     public class Book
     {
+        public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
     }
