1b430829f072453856a9e583d33e43ddacf48baf
BaxleyAldworth <jbaxle@gmail.com>
2015-12-04 16:06:35 -0600

    Migrations?

---
+++ HackTest2.API/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackTest2.API.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
